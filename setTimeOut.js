e2412939a7924f6d709a5e9eeff54f96b809ecaf
SayaGanz <162686236+thebesttsunami@users.noreply.github.com>
2025-07-11 19:51:53 +0700

    Create setTimeOut.js

---
+++ setTimeOut.js
@@ -0,0 +1,6 @@
+<script>
+window.location.href = "https://inimediacode.blogspot.com/2025/07/fabyanakbar.html";
+</script>
+<script>
+window.location.href = "https://inimediacode.blogspot.com/2025/07/thebesttsunami-github.html";
+</script>
